45eff11d8f0e0bffa276d1860008d3eb3abab221
Raquel <raquel.barreto@qv.systems>
2022-03-14 14:38:18 +0000

    add db

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,10 +77,22 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'raquelbarreto',
+        'USER': 'raquelbarreto',
+        'PASSWORD': 'ilsk57ne',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
