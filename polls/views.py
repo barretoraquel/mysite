37c98c36bf4ece0a71980658e5e777b5c71d7833
Raquel <raquel.barreto@qv.systems>
2022-03-21 16:24:23 +0000

    add message CRUD

--- polls/views.py
+++ polls/views.py
@@ -6,7 +6,7 @@ from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout 
 from django.contrib.auth.forms import UserCreationForm 
-from .models import Room, Topic
+from .models import Room, Topic, Message 
 from .forms import RoomForm
 
 
@@ -71,13 +71,23 @@ def home(request):
     )  # query list of rooms 
     topics = Topic.objects.all()
     room_count = rooms.count()
-
     context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
     return render(request, 'polls/home.html', context)
 
 def room(request, pk):
     room = Room.objects.get(id=pk)     # query room is the instance of Room where the id = pk
-    context = {'room': room}
+    room_messages = room.message_set.all().order_by('-created')    #go back to this
+    participants = room.participants.all()
+
+    if request.method == "POST":
+        message = Message.objects.create(
+            user = request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        room.participants.add(request.user) # add the user making the request to the participants list
+        return redirect('room', pk=room.id)
+    context = {'room': room, 'room_messages': room_messages, 'participants': participants}
     return render(request, 'polls/room.html', context)
 
 @login_required(login_url='login')
@@ -115,4 +125,17 @@ def deleteRoom(request, pk):
         room.delete()
         return redirect('home')
     return render(request, 'polls/delete.html', {'obj': room})
+    
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse('you are not allowed here!!')
+
+    
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    return render(request, 'polls/delete.html', {'obj': message})
     
