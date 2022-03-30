afc33697fd7c1e3f7e74a045fdd651dc94724ecc
Raquel <raquel.barreto@qv.systems>
2022-03-30 13:57:55 +0100

    fix activity feed

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'polls.apps.PollsConfig',
+    'tailwind',
 ]
 
 MIDDLEWARE = [
@@ -131,9 +132,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+STATICFILES_DIRS = [
+     BASE_DIR / 'static'
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- polls/urls.py
+++ polls/urls.py
@@ -1,5 +1,8 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include 
 from . import views
+from django.conf.urls.static import static 
+from django.conf import settings
 
 urlpatterns = [
     path('login/', views.loginPage, name='login'),
@@ -7,8 +10,12 @@ urlpatterns = [
     path('register/', views.registerPage, name='register'),
     path('', views.home, name="home"),
     path('room/<str:pk>/', views.room, name="room"),
+    path('profile/<str:pk>', views.userProfile, name="user-profile"),
     path('create-room/', views.createRoom, name="create-room"),
     path('update-room/<str:pk>', views.updateRoom, name="update-room"),
     path('delete-room/<str:pk>', views.deleteRoom, name="delete-room"),
-    path('delete-message/<str:pk>', views.deleteMessage, name="delete-message"),
+    path('delete-message/<str:pk>', views.deleteMessage, name="delete-message")
 ]
+
+if(settings.DEBUG):
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
